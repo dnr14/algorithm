e4b1f3944ae6e050d10f1693f8fc7a9a0035d42d
dnr14 <dnr14@naver.com>
2021-06-23 00:14:24 +0900

    변수 정리

--- rabinKrap.js
+++ rabinKrap.js
@@ -1,57 +1,56 @@
 
+// main
 (function () {
-  let source = "ababacabacaabacaaba";
-  let pattern = "abacaaba";
-  let patternHash = totalHash(pattern);
-  patternCheck(source, pattern, patternHash);
+  // let source = "ababacabacaabacaaba";
+  // let pattern = "abacaaba";
+  let source = "가나다라가나";
+  let pattern = "가나";
+  patternCheck(source, pattern);
 
 })();
 
 
-function totalHash(str) {
-  let value = 0;
-  for (let i = 0, j = (str.length - 1); j <= 0, i < str.length; j--, i++) {
-    let char = str[i].charCodeAt(0);
-    let hashNum = char * squared(1, 0, j);
-    value += hashNum;
-  }
-  return value;
-}
 
 function patternCheck(source, pattern) {
   let soruce_leng = source.length;
   let pattern_leng = pattern.length;
-  let patternHash = totalHash(pattern);
   let offset = pattern_leng;
+  let patternHash = strHash(pattern);
+  let sourceHash = 0;
   let hitCount = 0;
-  let hash = 0;
   let index = 0;
 
   while (1) {
     if (index === 0) {
-      let x = source.slice(index, offset);
-      hash = totalHash(x);
+      let sourceStr = source.slice(index, offset);
+      sourceHash = strHash(sourceStr);
     }
-    if (hash == patternHash) {
+    if (sourceHash == patternHash) {
       hitCount++;
       console.log(`${index + 1} 번째에서 발견했습니다.`);
       console.log(`source : ${source.slice(index, offset + index)} pattern : ${pattern}`);
     }
     let nextStrIndex = index + offset;
-    let 가장앞에있는문자 = source.slice(index, index + 1).charCodeAt(0) * Math.pow(2, offset - 1);
-    let 새롭게들어온문자 = source.slice(nextStrIndex, (nextStrIndex) + 1).charCodeAt(0) * Math.pow(2, 0);
-    hash = 2 * (hash - 가장앞에있는문자) + 새롭게들어온문자;
+    let firstStr = source.slice(index, index + 1).charCodeAt(0) * Math.pow(2, offset - 1);
+    let nextStr = source.slice(nextStrIndex, (nextStrIndex) + 1).charCodeAt(0) * Math.pow(2, 0);
+    sourceHash = 2 * (sourceHash - firstStr) + nextStr;
     if (index++ >= soruce_leng - pattern_leng) break;
   }
 
 }
 
-
-function squared(x, count, squaredCount) {
-  if (squaredCount === 0) return 1;
-  if (squaredCount === 1) return 2;
-  if (count === squaredCount) {
-    return x;
+function strHash(str) {
+  let hash = 0;
+  let index = 0;
+  let squared = str.length - 1;
+  while (index < str.length) {
+    let char = str[index].charCodeAt(0);
+    let hashNum = char * Math.pow(2, squared);
+    hash += hashNum;
+    index++;
+    squared--;
   }
-  return squared(x * 2, ++count, squaredCount);
+  return hash;
 }
+
+
